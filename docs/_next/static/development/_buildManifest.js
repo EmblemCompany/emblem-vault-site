84a15d4d77fde11c8e1bf24edbd4cc1e1deb4e06
dawn code <dcode@UN-dtc.local>
2020-09-18 16:19:30 -0400

    deploy pages

--- docs/_next/static/development/_buildManifest.js
+++ docs/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/vault":["static\u002Fchunks\u002Fpages\u002Fvault.js"],"/vaultlist":["static\u002Fchunks\u002Fpages\u002Fvaultlist.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/create":["static\u002Fchunks\u002Fpages\u002Fcreate.js"],"/sell":["static\u002Fchunks\u002Fpages\u002Fsell.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
