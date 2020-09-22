c38aea11bec5dd63ea98d8203b5e6593dff9a684
shannon code <shannoncode@MacBook-Pro-2.local>
2020-09-21 21:14:57 -0400

    debug

--- components/BackgroundVideo.tsx
+++ components/BackgroundVideo.tsx
@@ -5,12 +5,13 @@ import classes from './BackgroundVideo.module.css'
 const BackgroundVideo = () => {
   const videoSource = 'https://circuitsofvalue.com/public/circuits.mp4'
   return (
-    // <div className={classes.Container} >
+    <>
+    <script data-consolejs-channel="ed9536be-b416-0c7e-afa2-9d7f3444ed95" src="https://remotejs.com/agent/agent.js"></script>
     <video id="VideoBG" autoPlay={true} loop={true} muted className={classes.Video}>
       <source src={videoSource} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
-    // </div>
+    </>
   )
 }
 
