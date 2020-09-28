1aa3afe76f4de1f73efe51216abcef52796ffc32
dawn code <dcode@UN-dtc.local>
2020-09-28 15:19:29 -0400

    deploy pages

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "df3aef3b75b8d75039a5";
+/******/ 	var hotCurrentHash = "566d6e5b202fd4d5d476";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
