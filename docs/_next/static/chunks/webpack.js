5d2597cf93a30dba1aba3466fa8e367e554a2246
shannon code <shannoncode@MacBook-Pro-2.local>
2020-10-02 17:32:51 -0400

    deploy pages

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8fd09431c74736e57f35";
+/******/ 	var hotCurrentHash = "0ce22a13b949e3dcd8c7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- docs/_next/static/development/_buildManifest.js
+++ docs/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/next/dist/pages/_error":["static\u002Fchunks\u002Fpages\u002Fnext\u002Fdist\u002Fpages\u002F_error.js"],"/vault":["static\u002Fchunks\u002Fpages\u002Fvault.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/vault":["static\u002Fchunks\u002Fpages\u002Fvault.js"],"/vaults":["static\u002Fchunks\u002Fpages\u002Fvaults.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
