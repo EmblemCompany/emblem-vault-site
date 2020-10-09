4740456e032aece01866c8806be1a87f3a9842cd
shannon code <shannoncode@MacBook-Pro-2.local>
2020-10-09 14:50:25 -0400

    leaderboard

---
+++ pages/leaderboard.tsx
@@ -0,0 +1,13 @@
+import LeaderBoard from '../components/Leaderboard'
+import { Suspense } from 'react'
+import ErrorBoundary from '../components/ErrorBoundary'
+
+export default function VaultListPage(): JSX.Element {
+  return (
+    <Suspense fallback={<h1>Loading vaults...</h1>}>
+      <ErrorBoundary fallback={<h2>Oops! Something went wrong.</h2>}>
+        <LeaderBoard />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
