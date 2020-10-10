8b10da55d262ef2df2a5e14fa5ec32c8895b16f1
shannon code <shannoncode@MacBook-Pro-2.local>
2020-10-10 01:56:36 -0400

    deploy pages

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "51325f30b75f466b996a";
+/******/ 	var hotCurrentHash = "9fbd19c2383ee587a52b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- docs/_next/static/development/_buildManifest.js
+++ docs/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/nft":["static\u002Fchunks\u002Fpages\u002Fnft.js"],"/vault":["static\u002Fchunks\u002Fpages\u002Fvault.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/nft":["static\u002Fchunks\u002Fpages\u002Fnft.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
