9120dba3953b9e390406bc4ed2a526b139bd29da
shannon code <shannoncode@MacBook-Pro-2.local>
2020-12-13 14:47:37 -0500

    deploy pages

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "57e20eaad35262f6d5f0";
+/******/ 	var hotCurrentHash = "2e8fd2455a19779ca2ef";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
