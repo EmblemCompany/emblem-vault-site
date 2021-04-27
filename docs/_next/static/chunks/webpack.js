7e88dfeba52461538b5cb5d3bef2c8886e714975
shannon code <genecyber@gmail.com>
2021-04-27 14:14:45 -0400

    deploy pages

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "76dae11101f76be7abe2";
+/******/ 	var hotCurrentHash = "1c8ea15698a9f05a3292";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- docs/_next/static/development/_buildManifest.js
+++ docs/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/newest":["static\u002Fchunks\u002Fpages\u002Fnewest.js"],"/nft":["static\u002Fchunks\u002Fpages\u002Fnft.js"],"/nft/[id]":["static\u002Fchunks\u002Fpages\u002Fnft\u002F[id].js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/nft/[id]":["static\u002Fchunks\u002Fpages\u002Fnft\u002F[id].js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
