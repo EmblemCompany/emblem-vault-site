603216c9d35e2da682f9ce3e8a45d2c0fba465bf
shannon code <genecyber@gmail.com>
2023-02-12 09:22:59 -0500

    deploy pages

---
+++ docs/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/create":["static\u002Fchunks\u002Fpages\u002Fcreate.js"],"/nft":["static\u002Fchunks\u002Fpages\u002Fnft.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcreate","\u002Fnft"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
