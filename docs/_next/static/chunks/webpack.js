10d4bedb34270c70d305f6cb3ec81f313288d504
shannon code <genecyber@gmail.com>
2023-04-21 12:11:19 -0400

    deploy pages

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9198aa8d573674570dd5";
+/******/ 	var hotCurrentHash = "8e88f3d2e0938f001eda";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
