42f1fc95fdd3c1a2cf0abb41ae922faa9c8695ca
shannon code <genecyber@gmail.com>
2023-06-29 10:14:58 -0400

    deploy pages

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6d196a37ccd0172cfc55";
+/******/ 	var hotCurrentHash = "1904833b884787a99d4a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- docs/_next/static/development/_buildManifest.js
+++ docs/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/create":["static\u002Fchunks\u002Fpages\u002Fcreate.js"],"/createcurated":["static\u002Fchunks\u002Fpages\u002Fcreatecurated.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcreate","\u002Fcreatecurated"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
