bbe741bf65dac0bd400b305c0408ef272ecc5565
shannon code <genecyber@gmail.com>
2023-12-13 09:56:50 -0500

    hmm

--- components/Embed.tsx
+++ components/Embed.tsx
@@ -94,7 +94,7 @@ export class Embed extends Component<EmbedProps> {
             width="250px"
             height="300px"
           />
-        ) :  url.includes('/dynamic/') || url.includes('arweave') || url.includes('https://s3.amazonaws.com/') ||  url.includes('googleusercontent.com') || url.includes('ipfs.io') || url.includes('framed/') ||  url.includes('.png') || url.includes('.jpg') || url.includes('.jpeg') || url.includes('.gif') || url.includes('.svg') || url.includes('data:image') ? (
+        ) :  url.includes('/dynamic/') || url.includes('https://s3.amazonaws.com/') ||  url.includes('googleusercontent.com') || url.includes('ipfs.io') || url.includes('framed/') ||  url.includes('.png') || url.includes('.jpg') || url.includes('.jpeg') || url.includes('.gif') || url.includes('.svg') || url.includes('data:image') ? (
               <Image
                 p={"20px"}
                 h={"100%"}
--- components/embed/Generic.tsx
+++ components/embed/Generic.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import IframeResizer from 'iframe-resizer-react'
 
 type GenericProps = {
   url: string
@@ -8,7 +9,13 @@ export class Generic extends Component<GenericProps> {
   render() {
     return (
       <>
-        <iframe frameBorder="0" scrolling="no" src={this.props.url} width="100%" height="400" allow="encrypted-media"></iframe>
+        {/* <iframe frameBorder="0" scrolling="no" src=width="100%" height="400" allow="encrypted-media"></iframe> */}
+        <IframeResizer
+          log
+          src={this.props.url}        
+          scrolling={true}
+          width="100%" height="400" allow="encrypted-media"
+        />
       </>
     )
   }
